37c139c291ab5ca5ac4f4dfea9e61039ad0d3011
Diego <diegolparra@gmail.com>
2015-07-29 01:36:30 -0300

    = kamon-scalatra-example:change map to onComplete

--- kamon-examples/kamon-scalatra-example/src/main/scala/kamon/example/KamonSupport.scala
+++ kamon-examples/kamon-scalatra-example/src/main/scala/kamon/example/KamonSupport.scala
@@ -29,7 +29,7 @@ trait KamonSupport {
   def gauge[A](name: String)(thunk: => Long) = Kamon.metrics.gauge(name)(thunk)
   def time[A](name: String)(thunk: => A) = Latency.measure(Kamon.metrics.histogram(name))(thunk)
   def traceFuture[A](name:String)(future: => Future[A]):Future[A] = Tracer.withContext(Kamon.tracer.newContext(name)) {
-    future.map(f => Tracer.currentContext.finish())(SameThreadExecutionContext)
+    future.map(_ => Tracer.currentContext.finish())(SameThreadExecutionContext)
     future
   }
 }
